c56437059b3e8b2e110d9b8f35a7b40a29cee6a6
meddingt <mike@phed.org>
2009-10-21 11:58:04 +0000

    Marked as 0.12

--- PyDbgEng/setup.py
+++ PyDbgEng/setup.py
@@ -6,7 +6,7 @@ setup(name='PyDbgEng',
 	  author='Botten Biss, Michael Eddington',
 	  author_email='mike@phed.org',
 	  url='http://phed.org',
-	  version='0.11',
+	  version='0.12',
 	  packages=['PyDbgEng'],
 	  package_data={'PyDbgEng': ['data/*']},
 	  )
